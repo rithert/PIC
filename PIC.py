cb61615213c9c973e52dffa5cb0ba6663ee1140a
Rither <rither5711@gmail.com>
2020-06-15 14:49:25 -0500

    Creacion de la clase n clientes en servicio

--- PIC.py
+++ PIC.py
@@ -16,22 +16,66 @@ class modelo_matematico():
 
 
 
-
 #Clase hijo modelo PICS
-class PICS(modelo_matematico):
+class pICS(modelo_matematico):
     def __init__(self,Vlambda1,Vmiu1):
         self.__λ = Vlambda1
         self.__μ = Vmiu1
         super().__init__(self.__λ,self.__μ )
 
-#Probabilidad de hallar el sistema ocupado, utilización del sistema, probabilidad que tienen los usuarios de esperar para ser atendidos   
+    #Probabilidad de hallar el sistema ocupado, utilización del sistema, probabilidad que tienen los usuarios de esperar para ser atendidos   
     def func_ro(self):
             ro = self.__λ/self.__μ
-            return(ro)
-#La probabilidad de hallar el sistema vacío u ocioso, probabilidad que tienen los usuarios de no esperar o ser atendidos sin esperar en cola:
+            return(round(ro,4))
+
+    #La probabilidad de hallar el sistema vacío u ocioso, probabilidad que tienen los usuarios de no esperar o ser atendidos sin esperar en cola:
     def func_p0(self,ro):
         P0 = 1-(ro)
-        return(P0)
+        return(round(P0,4))
+
+
+    
+
+
+class PIC_prob_num_cliente():
+    def __init__(self,RO,P_0):
+        self.__ρ  = RO
+        self.__p0 = P_0
+        self.Pt = 0
+    
+
+    #La probabilidad de hallar exactamente n clientes dentro del sistema
+    def func_nclientes(self,n):
+        Pn = self.__p0 *( self.__ρ**n)
+        return (round(Pn,4))
+
+    #La probabilidad de hallar exactamente n clientes dentro del sistema, valores(lista) ingresdos por el cliente
+    def func_nclientes_range(self,ro,P0,lista):
+        for i in  lista:
+            Pn = P0*(ro**i)
+            self.Pt += Pn
+        return (round(Pn,4))
+    
+    #La probabilidad de hallar exactamente n clientes dentro del sistema rango[x-n]
+    def func_nclientes_range(self,ro,P0,Xinicial,Xfinal):
+        lista = list(range(Xinicial,Xfinal))
+        for i in  lista:
+            Pn = P0*(ro**i)
+            self.Pt += Pn
+        return (round(Pn,4))
+
+    #La probabilidad de hallar exactamente al menos n clientes dentro del sistema 
+    def func_nclientes_range(self,ro,P0,Xinicial):
+        lista = list(range(Xinicial,0,-1))
+        for i in  lista:
+            Pn = P0*(ro**i)
+            self.Pt += Pn
+        Almenos_nclientes= 1-self.Pt
+        return (round(Almenos_nclientes,4))
+
+
+
+
 
 
 
@@ -49,10 +93,39 @@ while True:
         print("Ingrese numeros")
         continue
 
-print("mama")
+
+
 if ObjPICS.condicion_estabilidad():
     valor_ro = ObjPICS.func_ro()
     print ("LA probabilidad de encontar el sistema ocupado es de ro : ", valor_ro)
     valor_inve_ro = ObjPICS.func_p0(valor_ro)
     print("La probabilidad de hallar el sistema vacio ese de: ", valor_inve_ro)
-    
+
+
+
+
+prob_Total = 0
+
+print("""Menu
+        1. Probabilidad de usuarios en el sistema
+        2. Probabilidad de usuarios en cola 
+        """)
+
+opcion = int(input("Ingrese la una opcion "))
+if opcion ==1:
+    while(True):
+        print ("La probabilidad de hallar exactamente n clientes dentro del sistema")
+        n_clientes = int(input("Ingrese el numero de clientes EN EL SISTEMA "))
+        proba_n_clientes = ObjPICS.func_nclientes(valor_ro,valor_inve_ro,n_clientes)
+        print(f"Probabiliad de hallar {n_clientes} clientes: {proba_n_clientes}")
+        prob_Total += proba_n_clientes
+        seguir = input("Si desea parar ingrese *")
+        if seguir == "*":
+            break
+        else:
+            continue
+    print("Probilidad total: ", prob_Total)
+            
+else:
+    prob_Total += proba_n_clientes
+    print ("Probabilidad total:", prob_Total)
